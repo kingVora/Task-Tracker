0b1f7bcb9a1ee7ed188764c54086afc17e888a24
kingVora <vparth2003@gmail.com>
2025-03-27 00:46:47 +0530

    Delete frontend folder

--- task-tracker-frontend/src/layouts/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export const Dashboard = () => {
-    return (
-        <div className="p-4">
-          <h1 className="text-2xl font-bold">Dashboard</h1>
-          <p>Welcome to your task tracking app!</p>
-        </div>
-      );
-};
--- task-tracker-frontend/src/layouts/TaskLog.tsx
+++
@@ -1,8 +0,0 @@
-export const TaskLog = () => {
-    return (
-        <div className="p-4">
-          <h1 className="text-2xl font-bold">Task Log</h1>
-          <p>View all tasks here.</p>
-        </div>
-      );
-};
