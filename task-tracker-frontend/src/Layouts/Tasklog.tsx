e4ff6caa3d8a5f65465ea63860f12c502329fa72
kingVora <vparth2003@gmail.com>
2025-03-27 01:08:15 +0530

    Use vite as well and create basic Dashboard and tasklog layouts

---
+++ task-tracker-frontend/src/Layouts/Tasklog.tsx
@@ -0,0 +1,8 @@
+export const TaskLog = () => {
+    return (
+        <div className="p-6">
+          <h1 className="text-2xl font-bold">Task Log</h1>
+          <p>View and manage your logged tasks.</p>
+        </div>
+      );
+}
---
+++ task-tracker-frontend/src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./Layouts/Dashboard";
+import { TaskLog } from "./Layouts/Tasklog";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/task-log" element={<TaskLog />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
