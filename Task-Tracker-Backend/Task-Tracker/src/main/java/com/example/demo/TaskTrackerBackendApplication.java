61dfdab07f38273fa5675329434435e0068c513e
kingVora <vparth2003@gmail.com>
2025-03-27 21:53:28 +0530

    Add env file

--- Task-Tracker-Backend/Task-Tracker/src/main/java/com/example/demo/TaskTrackerBackendApplication.java
+++ Task-Tracker-Backend/Task-Tracker/src/main/java/com/example/demo/TaskTrackerBackendApplication.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class TaskTrackerBackendApplication {
 
 	public static void main(String[] args) {
+		String projectRoot = Paths.get(System.getProperty("user.dir"), "task-tracker").toString();
+		Dotenv dotenv = Dotenv.configure().directory(projectRoot).ignoreIfMissing().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(TaskTrackerBackendApplication.class, args);
 	}
 
