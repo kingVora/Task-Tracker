61cfdfa1500be495c8ad5199a4776c2fb74b0b68
kingVora <vparth2003@gmail.com>
2025-05-18 19:00:17 +0530

    Create AppHeader and toggle theme feature

--- task-tracker-frontend/src/Utils/LeftSideBar/Components/SideBar.tsx
+++ task-tracker-frontend/src/Utils/LeftSideBar/Components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { Link, useLocation } from "react-router";
 import { useSidebar } from "../../../Context/SidebarContext";
 import {
