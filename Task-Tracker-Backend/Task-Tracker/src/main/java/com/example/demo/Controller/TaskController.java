debc5ec9347c44bba01da0031ef55952e5bd4ac4
kingVora <vparth2003@gmail.com>
2025-06-11 21:50:07 +0530

    Check task status and conditionally update it to overdue while fetching tasks

--- Task-Tracker-Backend/Task-Tracker/src/main/java/com/example/demo/Controller/TaskController.java
+++ Task-Tracker-Backend/Task-Tracker/src/main/java/com/example/demo/Controller/TaskController.java
@@ -4,8 +4,14 @@ import com.example.demo.Entities.Task;
 import com.example.demo.Service.TaskService;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @RestController
 public class TaskController {
@@ -18,7 +24,32 @@ public class TaskController {
 
     @GetMapping("/tasks")
     public List<Task> getTasks(){
-        return service.fetchTasks();
+        List<Task> fetchedTasks = service.fetchTasks();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        fetchedTasks.forEach(task -> {
+            try {
+                if(task.getStatus().equals("due")) {
+                    System.out.println("Inside getTasks");
+                    System.out.println(task.getTitle());
+                    Date today = sdf.parse(LocalDate.now().toString());
+                    Date due = sdf.parse(task.getDueDate());
+
+                    TimeUnit time = TimeUnit.DAYS;
+
+                    double difference = time.convert(today.getTime() - due.getTime(), TimeUnit.MILLISECONDS);
+                    System.out.println("The difference is" + difference);
+
+                    if (difference > 0) {
+                        System.out.println("Task is now Overdue!!");
+                        task.setStatus("overdue");
+                        service.editTask(task.getTaskId(), task);
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+        return fetchedTasks;
     }
 
     @GetMapping("/totaltasks")
